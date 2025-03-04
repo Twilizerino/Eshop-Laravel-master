279b6a2535c8c35dabb62171102199386efa4e82
Twilizerino <jasa.steam@seznam.cz>
2025-03-04 07:31:00 +0100

    Laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 
+
+return (
+    
+)
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', static function () {
+    return view('home');
 });
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
